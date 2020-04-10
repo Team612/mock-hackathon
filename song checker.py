26258db98fd325adda2efd1916acf0a5cccf0f85
myrrhkhan <57461851+myrrhkhan@users.noreply.github.com>
2020-04-10 01:33:44 -0400

    created this schnit in 10 minutes lmao

---
+++ song checker.py
@@ -0,0 +1,201 @@
+lyric=input("type a line from a song...")
+
+#below i was gonna put every lyric to my favorite song but i was too lazy so i just put two songs.
+favlyrics="""
+I'm the son of rage and love
+The Jesus of Suburbia
+The bible of none of the above
+On a steady diet of
+Soda Pop and Ritalin
+No one ever died for my
+Sins in hell
+As far as I can tell
+At least the ones that I got away with
+And there's nothing wrong with me
+This is how I'm supposed to be
+In a land of make believe
+That don't believe in me
+Get my television fix
+Sitting on my crucifix
+The living room in my private womb
+While the Moms and Brads are away
+To fall in love and fall in debt
+To alcohol and cigarettes
+And Mary Jane
+To keep me insane
+Doing someone else's cocaine
+And there's nothing wrong with me
+This is how I'm supposed to be
+In a land of make believe
+That don't believe in me
+At the center of the earth
+In the parking lot
+Of the 7-11 where I was taught
+The motto was just a lie
+It says home is where your heart is
+But what a shame
+Cause everyone's heart
+Doesn't beat the same
+It's beating out of time
+City of the dead
+At the end of another lost highway
+Signs misleading to nowhere
+City of the damned
+Lost children with dirty faces today
+No one really seems to care
+I read the graffiti
+In the bathroom stall
+Like the holy scriptures of a shopping mall
+And so it seemed to confess
+It didn't say much
+But it only confirmed that
+The center of the earth
+Is the end of the world
+And I could really care less
+City of the dead
+At the end of another lost highway
+Signs misleading to nowhere
+City of the damned
+Lost children with dirty faces today
+No one really seems to care
+I don't care if you don't
+I don't care if you don't
+I don't care if you don't care
+I don't care
+Everyone's so full of shit
+Born and raised by hypocrites
+Hearts recycled but never saved
+From the cradle to the grave
+We are the kids of war and peace
+From Anaheim to the Middle East
+We are the stories and disciples of
+The Jesus of suburbia
+Land of make believe
+And it don't believe in me
+Land of make believe
+And I don't believe
+And I don't care!
+Dearly beloved are you listening?
+I can't remember a word that you were saying
+Are we demented or am I disturbed?
+The space that's in between insane and insecure
+Oh therapy, can you please fill the void?
+Am I retarded or am I just overjoyed
+Nobody's perfect and I stand accused
+For lack of a better word, and that's my best excuse
+To live, and not to breathe
+Is to die, in tragedy
+To run, to run away
+To find, what you believe
+And I leave behind
+This hurricane of fucking lies
+I lost my faith to this
+This town that don't exist
+So I run, I run away
+To the lights of masochists
+And I, leave behind
+This hurricane of fucking lies
+And I, walked this line
+A million and one fucking times
+But not this time
+I don't feel any shame
+I wont apologize
+When there ain't nowhere you can go
+Running away from pain
+When you've been victimized
+Tales from another broken home
+Oh you're leaving
+You're leaving
+You're leaving
+Are you leaving home?
+
+Robert's got a quick hand
+He'll look around the room, he won't tell you his plan
+He's got a rolled cigarette, hanging out his mouth he's a cowboy kid
+Yeah found a six shooter gun
+In his dad's closet hidden oh in a box of fun things, I don't even know what
+But he's coming for you, yeah he's coming for you
+All the other kids with the pumped up kicks
+You'd better run, better run, out run my gun
+All the other kids with the pumped up kicks
+You'd better run, better run, faster than my bullet
+All the other kids with the pumped up kicks
+You'd better run, better run, out run my gun
+All the other kids with the pumped up kicks
+You'd better run, better run, faster than my bullet
+Daddy works a long day
+He be coming home late, he's coming home late
+And he's bringing me a surprise
+'Cause dinner's in the kitchen and it's packed in ice
+I've waited for a long time
+Yeah the slight of my hand is now a quick pull trigger
+I reason with my cigarette
+And say your hair's on fire, you must have lost your wits, yeah
+All the other kids with the pumped up kicks
+You'd better run, better run, out run my gun
+All the other kids with the pumped up kicks
+You'd better run, better run, faster than my bullet
+All the other kids with the pumped up kicks
+You'd better run, better run, out run my gun
+All the other kids with the pumped up kicks
+You'd better run, better run, faster than my bullet
+All the other kids with the pumped up kicks
+You'd better run, better run, out run my gun
+All the other kids with the pumped up kicks
+You'd better run, better run, faster than my bullet
+All the other kids with the pumped up kicks
+You'd better run, better run, out run my gun
+All the other kids with the pumped up kicks
+You'd better run, better run, faster than my bullet
+All the other kids with the pumped up kicks
+You'd better run, better run, out run my gun
+All the other kids with the pumped up kicks
+You'd better run, better run, faster than my bullet
+
+Yeah it's over, it's over
+I'm circling these vultures
+Got me praying, man this hunger
+Feeling something rotten
+Last time I saw you said "What's up?"
+And pushed right through
+Then I tried to catch you
+But we're always on the move
+And now it's over, we're sober
+Symptoms of the culture
+And the night ain't getting younger
+Last call's around the corner
+Feeling kinda tempted
+And I'm pouring out the truth
+Just fading out these talkers 'cause now all I want is you, I'm saying
+Come over here sit next to me
+We can see where things go naturally
+Just say the word and I'll part the sea
+Just come over here and sit next to me
+And I'll take you high, high
+Well I ain't frontin' my intention
+Got your man outlined in chalk
+It's a midnight intervention
+Got no plans to make it stop
+Last time I saw you said you wanna keep it light
+But I'm here to tell you
+I'm not trying to change your mind
+It's alright, ooh, it's alright, ooh
+Yeah, come over here and sit next to me
+We can see where things go naturally
+Just say the word and I'll part the sea
+Just come over here and sit next to me
+And I'll take you high, high, high, high
+It's alright
+It's alright
+Yeah, it's alright
+Yeah, it's alright
+So come over here and sit next to me
+We can see where things go naturally
+Just say the word and I'll part the sea
+Just come over here and sit next to me, ooh
+"""
+
+if lyric.lower() in favlyrics.lower(): #to make sure that the entry isn't case sensitive.
+    print("This lyric is from one of my favorite songs!")
+else:
+    print("i don't recognize this schnit")
